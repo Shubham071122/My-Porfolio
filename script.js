2acebd893ab092e88fe11f60a40ff27b35256bdd
Shubham kumar <shubhamkumar7112002@gmail.com>
2024-01-13 15:50:39 +0530

    add contact form

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+
+
 const navToggle = document.querySelector('.nav-toggle');
 const navItem = document.querySelector(".nav-items")
-
-
 
 navToggle.addEventListener("click", () => {
   if (navItem.classList.contains("active")) {
@@ -16,4 +16,56 @@ navToggle.addEventListener("click", () => {
 //   if(navItem.classList.contains('active'))
 //     navItem.classList.remove('active');
 // })
+
+
+//** Handle popup */
+
+const form = document.querySelector('.form');
+const result = document.querySelector('#form-result');
+const setCaptcha = document.querySelector('.h-captcha');
+
+//on submit, POST to web3Forms and get response
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+  //fetch result
+  fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: json
+  })
+    .then(async (response) => {//show response
+      let json = await response.json();
+      console.log(json);
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+        result.classList.add("notice--success");
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+        result.classList.add("notice--warning");
+
+      }
+    })
+    .catch(error => {//show error
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+      result.classList.add("notice--danger");
+
+    })
+    .then(function () {//reset form
+      form.reset();
+      //make response dissapear after 3 seconds if you wish
+      setTimeout(() => {
+        result.style.display = "none";
+        location.reload();
+      }, 3000);
+    });
+});
 
